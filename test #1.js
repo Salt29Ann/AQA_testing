f279b3bd1c6dec728c774652c42ef78884c017f0
Salt29Ann <ann2902sa@gmail.com>
2024-02-01 19:03:53 +0800

    added_base_file

---
+++ test #1.js
@@ -0,0 +1,13 @@
+// var myfirstvariable = 4
+// let my2Var = ''
+// const myURL = "http"
+
+// console.log(myfirstvariable) //вывод значения
+// console.log('myfirstvariable') //вывод текста из стринги
+// console.log("my first variable has value " + myfirstvariable) //вывод текста из стринги мо значением
+// console.log(Boolean("false"))
+// console.log(Number(Boolean("false"))) //булиан значение приведение к номеру
+// console.log(myfirstvariable+"my2Var"). //значение+стринга
+// console.log(Number('5')+Number("5")) //приведение к номеру
+
+
